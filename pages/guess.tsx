effba81b798e1883e96d530c0e46867eae16e124
Dee Bryant <dee@tolka.io>
2022-07-11 20:12:34 +1000

    feat(guessed): created guessing game

---
+++ pages/guess.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState, useRef } from "react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Confetti from "react-dom-confetti";
+import { useReward } from "react-rewards";
+
+const Guessed: NextPage = () => {
+  const catchphrase = [
+    {
+      name: "rainbow",
+      code: "🌈",
+    },
+    {
+      name: "baloon",
+      code: "🎈",
+    },
+    {
+      name: "unicorn",
+      code: "🦄",
+    },
+    {
+      name: "heart",
+      code: "💗",
+    },
+  ];
+
+  const config = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 40,
+    elementCount: 70,
+    dragFriction: 0.12,
+    duration: 3000,
+    stagger: 3,
+    width: "10px",
+    height: "10px",
+    perspective: "500px",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+  };
+
+  const [guessed, setGuessed] = useState("");
+  const [interaction, setInteraction] = useState(catchphrase[0].code);
+  const [confetti, setConfetti] = useState(false); //This contains the current time on the counter in seconds
+  const { reward: confettiReward, isAnimating: isConfettiAnimating } =
+    useReward("confettiReward", "emoji", { emoji: [interaction] });
+
+  const onChange = (e) => {
+    const guess = e.target.value;
+    setGuessed(guess);
+    const result = catchphrase.find((el) => el.name === guess);
+    if (result) {
+      setConfetti(true);
+      setInteraction(result.code);
+      confettiReward();
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pomorodro</title>
+        <meta name="Guess the secret word" content="Guess the secret word" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header style={{ padding: "2rem" }}>
+        <Link href="/">&larr; home</Link>
+      </header>
+      <main className={styles.main}>
+        <div>
+          <header>
+            <h1 className={styles.rainbow}>Guess the secret code</h1>
+          </header>
+        </div>
+        <span id="confettiReward" />
+        <input
+          id="guessed"
+          value={guessed}
+          onChange={onChange}
+          onFocus={(e) => (e.target.placeholder = "")}
+        />
+        <p id="result"></p>
+        <Confetti active={confetti} config={config} />
+      </main>
+    </div>
+  );
+};
+
+export default Guessed;
